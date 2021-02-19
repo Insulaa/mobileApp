f074191abcd188eca9870664acf88eb95630cd81
emakarsk <55766172+emakarsk@users.noreply.github.com>
2021-02-18 20:01:06 -0500

    glucose data api and props for glucose icons

--- src/components/GlucoseReadingIcon/GlucoseReadingIcon.js
+++ src/components/GlucoseReadingIcon/GlucoseReadingIcon.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import styles from './styles';
 import {View, Text} from 'react-native';
+import PropTypes from 'prop-types';
 
-const GlucoseReadingIcon = () => {
+const GlucoseReadingIcon = (props) => {
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <View style={styles.iconContainer}>
-          <Text style={styles.numberText}>10.0</Text>
-          <Text style={styles.unitText}>mg/dL</Text>
+          <Text style={styles.numberText}>{props.glucoseReading}</Text>
+          <Text style={styles.unitText}>{props.unit}</Text>
         </View>
-        <Text style={styles.timeText}>12:20 AM</Text>
+        <Text style={styles.timeText}> {props.time} </Text>
       </View>
     </View>
   );
+};
+
+GlucoseReadingIcon.propTypes = {
+  glucoseReading: PropTypes.number,
+  time: PropTypes.string,
+  unit: PropTypes.string,
 };
 
 export default GlucoseReadingIcon;
--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -15,23 +15,52 @@ import GlucoseInputScreen from '../GlucoseInputScreen/GlucoseInputScreen';
 import {withNavigation} from 'react-navigation';
 import GlucoseScreenButton from '../Buttons/GlucoseScreenButton';
 import GlucoseReadingIcon from '../GlucoseReadingIcon/GlucoseReadingIcon';
+import {useEffect} from 'react';
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   let [user, setUser] = React.useState('');
   let [glucoseReadings, setGlucoseReadings] = React.useState([]);
+
+  useEffect(() => {
+    getUserGlucoseData();
+  }, []);
+
+  const getUserGlucoseData = () => {
+    axios
+      .get('http://10.0.2.2:8000/views/glucoseLevels/?user_id=1')
+      .then((response) => {
+        const glucoseList = response.data;
+        console.log(glucoseList);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Glucose Levels</Text>
       <Text style={styles.body}>Todays Readings</Text>
       <View style={styles.glucoseReadingsContainer}>
-        <GlucoseReadingIcon />
-        <GlucoseReadingIcon />
-        <GlucoseReadingIcon />
+        <GlucoseReadingIcon
+          glucoseReading={12.2}
+          time="10:06 AM"
+          unit="mg/dL"
+        />
+        <GlucoseReadingIcon
+          glucoseReading={10.5}
+          time="12:26 PM"
+          unit="mg/dL"
+        />
+        <GlucoseReadingIcon
+          glucoseReading={14.1}
+          time="06:35 PM"
+          unit="mg/dL"
+        />
       </View>
       <Text style={styles.body}>14 Day Average</Text>
       <View style={styles.glucoseReadingsContainer}>
-        <GlucoseReadingIcon />
+        <GlucoseReadingIcon glucoseReading={11.7} time="" unit="mg/dL" />
       </View>
       <View style={styles.buttonsContainer}>
         <GlucoseScreenButton buttonText="ADD READING" />
