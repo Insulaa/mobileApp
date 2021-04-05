f78aca75e48407e6e55b1157f0f8bfd0dca24849
Calvin <calvincheung66@yahoo.ca>
2021-04-05 18:18:32 -0400

    auth stuff

--- src/api/services.ts
+++ src/api/services.ts
@@ -3,6 +3,7 @@ import MedicationService from './medicationService';
 import UserProfileService from './userProfileService';
 import UserService from './userService';
 import BloodPressureService from './bloodPressureService';
+import UserService from './userService';
 
 const createServices = () => ({
   glucoseService: new GlucoseService(),
@@ -10,6 +11,7 @@ const createServices = () => ({
   userProfileService: new UserProfileService(),
   userService: new UserService(),
   bloodPressureService: new BloodPressureService(),
+  userService: new UserService(),
 });
 
 export default createServices;
