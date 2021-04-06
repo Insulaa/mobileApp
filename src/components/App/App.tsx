d9b95563d08301ad61ef1fcfdea39ff4bc3d5b65
unknown <emakarsk@gmail.com>
2021-04-06 16:43:36 -0400

    fix initial route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -92,7 +92,7 @@ const App = () => {
     <NavigationContainer>
       <Provider store={store}>
         <ServicesContext.Provider value={services}>
-          <Stack.Navigator initialRouteName="Home">
+          <Stack.Navigator initialRouteName="Auth">
             <Stack.Screen
               name="Auth"
               component={Auth}
