bb9bea6bc9e993c438aeb7b76ea8d775531a4afd
unknown <emakarsk@gmail.com>
2021-04-05 22:06:50 -0400

    added apis, store, and local states

---
+++ src/api/bloodPressureService.ts
@@ -0,0 +1,55 @@
+import qs from 'query-string';
+import axios from 'axios';
+
+export type BloodPressureReading = {
+  id: number;
+  date: string;
+  systolic: number;
+  diastolic: number;
+  timestamp: string;
+  patient: number;
+};
+
+class BloodPressureService {
+  public async getBloodPressureReadings(props: {patientId: number}) {
+    const {patientId} = props;
+    const apiUrl = qs.stringifyUrl({
+      url: 'http://10.0.2.2:8000/views/BloodPressure/',
+      query: {
+        patient: patientId,
+      },
+    });
+    try {
+      const response = await axios.get<BloodPressureReading[]>(apiUrl);
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async addBloodPressureReading(props: {
+    patientId: number;
+    systolic: number;
+    diastolic: number;
+  }) {
+    const {patientId, systolic, diastolic} = props;
+    const apiUrl = 'http://10.0.2.2:8000/views/glucoseLevels/';
+    const body = {
+      systolic: systolic,
+      diastolic: diastolic,
+      patient: patientId,
+    };
+    try {
+      const response = await axios.post(apiUrl, body);
+      if (response.status === 201) {
+        return response.data;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default BloodPressureService;
--- src/api/services.ts
+++ src/api/services.ts
@@ -1,11 +1,13 @@
 import GlucoseService from './glucoseService';
 import MedicationService from './medicationService';
 import UserProfileService from './userProfileService';
+import BloodPressureService from './bloodPressureService';
 
 const createServices = () => ({
   glucoseService: new GlucoseService(),
   medicationService: new MedicationService(),
   userProfileService: new UserProfileService(),
+  bloodPressureService: new BloodPressureService(),
 });
 
 export default createServices;
